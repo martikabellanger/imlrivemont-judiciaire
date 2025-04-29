3aeb4495c0c628b9232db30eedf6720b07035c54
martikabellanger <oranevercruysse@gmail.com>
2025-04-29 10:47:48 +0200

    Update script-demande.js

--- js/script-demande.js
+++ js/script-demande.js
@@ -3,7 +3,7 @@ document.getElementById('demande-form').addEventListener('submit', function(e) {
     const medecin = document.getElementById('ref-medecin-doc').value.trim();
     const cle = document.getElementById('cle-auth-doc').value.trim();
     if (medecin === '5523180096' && cle === 'vanterin_rivemont_2008') {
-        window.location.href = 'resultat.html';
+        window.location.href = 'allergologie.html';
     } else {
         document.getElementById('error-message-doc').textContent = 'Erreur dans les informations saisies.';
     }
